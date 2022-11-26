bad1519345b2a5a7a6c1b612ad4ce61fd00666cd
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2022-11-26 12:30:18 +0000

    テスト前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,13 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "Book flashwas successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all.order(created_at: :desc)
+      render :index
+    end
   end
 
   def show
@@ -19,14 +24,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
