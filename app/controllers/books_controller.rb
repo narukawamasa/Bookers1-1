efad967c5514a973f7f06cf04a877907deba863c
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2022-11-26 11:31:12 +0000

    投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,12 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def show
@@ -7,4 +14,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
